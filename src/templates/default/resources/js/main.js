9e17273e649105e0da98ef04769c26abde3b6bdc
atilafassina <hey@atilafassina.com>
2014-05-18 18:43:19 -0300

    add license / add domain to config.json / reduce logo.svg size

--- src/templates/default/resources/js/main.js
+++ src/templates/default/resources/js/main.js
@@ -1,1 +1,8 @@
-
+(function(){
+    'use strict';
+    [].slice.call(document.getElementsByTagName('a')).forEach(function(element) {
+        if(element.href.indexOf(document.location.host) == -1) {
+            element.target = '_blank';
+        }
+    });
+})();
