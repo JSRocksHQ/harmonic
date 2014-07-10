46d4fa527e9d783b3e06bc48b1541d9fced37121
Jaydson Gomes <jayalemao@gmail.com>
2014-07-10 02:21:48 -0300

    Missing stylus on dependecies. Added node engine

--- bin/cli/logo.js
+++ bin/cli/logo.js
@@ -1,6 +1,6 @@
 var logo = "\n" +
 "|_| _  _ _ _  _  _ . _ \n" +
 "| |(_|| | | |(_)| ||(_ \n" +
-"                   " + GLOBAL.version +"  \n" +
+"                 " + GLOBAL.version +"  \n" +
 "";
 module.exports = logo;
