c23ce44dcb042571d7af5e07377dacb802557e1e
Jaydson Gomes <jayalemao@gmail.com>
2014-07-15 22:42:30 -0300

    Removing unnecessary src dir. Also fix issue #40

--- src/templates/default/resources/js/main.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    'use strict';
-    [].slice.call(document.getElementsByTagName('a')).forEach(function(element) {
-        if(element.href.indexOf(document.location.host) == -1) {
-            element.target = '_blank';
-        }
-    });
-})();
