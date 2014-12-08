0197de5acfd63746f729fe0039bdac61e2db3830
Jaydson <jaydson.gomes@corp.terra.com.br>
2014-12-08 18:46:14 -0200

    Fixing path redeclaration. JSHint unused was set to false

--- src/bin/config.js
+++ src/bin/config.js
@@ -1,10 +1,12 @@
+ /*jshint unused:false*/
 let path = require('path');
 let fs = require('fs');
-let rootdir = path.normalize(__dirname + '/../');
-let pack = JSON.parse(fs.readFileSync(rootdir + '/../package.json').toString());
 
 // rootdir === `dist` dir
-export let rootdir = rootdir;
+let exportRootDir = path.normalize(__dirname + '/../');
+let pack = JSON.parse(fs.readFileSync(exportRootDir + '/../package.json').toString());
+
+export let rootdir = exportRootDir;
 export let version = pack.version;
 export let postspath = path.normalize('./src/posts/');
 export let pagespath = path.normalize('./src/pages/');
