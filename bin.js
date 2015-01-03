3dbd86e5805c5095a5bf6bc1cb2c98f2dacc9c8b
UltCombo <ultcombo@gmail.com>
2015-01-03 09:58:13 -0200

    Attempt to fix #93

---
+++ bin.js
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+require('v8-argv')('--harmony', __dirname + '/dist/bin/cli/program');
--- src/harmonic.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-require('v8-argv')('--harmony', __dirname + '/bin/cli/program');
