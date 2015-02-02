b040ba46b3c3af53d35ba9248091cf32fca12a9b
UltCombo <ultcombo@gmail.com>
2015-02-02 18:06:39 -0200

    Add temporary workaround for #113

--- src/bin/client/harmonic-client.js
+++ src/bin/client/harmonic-client.js
@@ -10,6 +10,11 @@
 // [BUG] https://github.com/jscs-dev/node-jscs/issues/706
 // jscs: disable disallowSpacesInFunctionExpression, disallowSpacesInAnonymousFunctionExpression
 
+
+// slush-es20xx doesn't support front-end transpiling yet,
+// so this file is transpiled manually for the time being
+
+/*
 class Harmonic {
 
     constructor(name) {
@@ -28,3 +33,41 @@ class Harmonic {
         return __HARMONIC.PAGES__;
     }
 }
+*/
+
+
+"use strict";
+
+var _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
+
+var Harmonic = (function () {
+    function Harmonic(name) {
+        this.name = name;
+    }
+
+    _prototypeProperties(Harmonic, null, {
+        getConfig: {
+            value: function getConfig() {
+                return __HARMONIC.CONFIG__;
+            },
+            writable: true,
+            configurable: true
+        },
+        getPosts: {
+            value: function getPosts() {
+                return __HARMONIC.POSTS__;
+            },
+            writable: true,
+            configurable: true
+        },
+        getPages: {
+            value: function getPages() {
+                return __HARMONIC.PAGES__;
+            },
+            writable: true,
+            configurable: true
+        }
+    });
+
+    return Harmonic;
+})();
