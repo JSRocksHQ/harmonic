ac916e412b54ac5dea70b86d86398ab3c6c4cb07
Andre Alves Garzia <contato@andregarzia.com>
2015-04-16 14:39:54 -0300

    Enhancement to enable running harmonic commands from subfolders

--- src/bin/cli/harmonic.js
+++ src/bin/cli/harmonic.js
@@ -1,6 +1,6 @@
 import program from 'commander';
 import { version } from '../config';
-import { cliColor } from '../helpers';
+import { cliColor, findHarmonicRoot } from '../helpers';
 import logo from './logo';
 import { init, config, newFile, run } from './util';
 import { build } from '../core';
@@ -12,7 +12,7 @@ program
 program
     .command('init [path]')
     .description('Init your static website')
-    .action((path = '.') => {
+    .action((path = findHarmonicRoot('.')) => {
         console.log(logo);
         init(path);
     });
@@ -20,7 +20,7 @@ program
 program
     .command('config [path]')
     .description('Config your static website')
-    .action(async (path = '.') => {
+    .action(async (path = findHarmonicRoot('.')) => {
         console.log(logo);
         await config(path);
         console.log(clc.info('\nharmonic.json successfully updated.'));
@@ -29,7 +29,7 @@ program
 program
     .command('build [path]')
     .description('Build your static website')
-    .action((path = '.') => {
+    .action((path = findHarmonicRoot('.')) => {
         build(path);
     });
 
@@ -37,7 +37,7 @@ program
     .command('new_post <title> [path]')
     .option('--no-open', 'Don\'t open the markdown file(s) in editor')
     .description('Create a new post')
-    .action((title, path = '.', { open: autoOpen }) => {
+    .action((title, path = findHarmonicRoot('.'), { open: autoOpen }) => {
         newFile(path, 'post', title, autoOpen);
     });
 
@@ -45,7 +45,7 @@ program
     .command('new_page <title> [path]')
     .option('--no-open', 'Don\'t open the markdown file(s) in editor')
     .description('Create a new page')
-    .action((title, path = '.', { open: autoOpen }) => {
+    .action((title, path = findHarmonicRoot('.'), { open: autoOpen }) => {
         newFile(path, 'page', title, autoOpen);
     });
 
@@ -53,7 +53,7 @@ program
     .command('run [port] [path]')
     .option('--no-open', 'Don\'t open a new browser window')
     .description('Run you static site locally. Port is optional')
-    .action((port = 9356, path = '.', { open: autoOpen }) => {
+    .action((port = 9356, path = findHarmonicRoot('.'), { open: autoOpen }) => {
         build(path).then(function() {
             run(path, port, autoOpen);
         });
--- src/bin/helpers.js
+++ src/bin/helpers.js
@@ -1,8 +1,10 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import _cliColor from 'cli-color';
+import { resolve } from 'path';
 
-export { cliColor, isHarmonicProject, getConfig, titleToFilename };
+
+export { cliColor, isHarmonicProject, getConfig, titleToFilename, findHarmonicRoot };
 
 // CLI color
 function cliColor() {
@@ -14,20 +16,54 @@ function cliColor() {
     };
 }
 
+// Friendly message for non-initialized folder
+function displayNonInitializedFolderErrorMessage() {
+    const clc = cliColor();
+
+    console.log(
+        clc.warn('It seems this is not an Harmonic project yet. \n') +
+        clc.warn('Check your directory or run ') +
+        clc.info.bgWhite.italic(' harmonic init ') +
+        clc.warn(' to start a new Harmonic project.')
+    );
+}
+
+// Find harmonic.json
+function findHarmonicRoot(sitePath) {
+    
+    const clc = cliColor();
+    var currentPath = resolve(sitePath);
+    var oldPath = "";
+
+
+    while (!isHarmonicProject(currentPath)) {
+        // TODO: Climb folders up until find harmonic.json file, if can't find then error.
+
+        oldPath = currentPath;
+        currentPath = resolve(currentPath,"..");
+
+        if (oldPath === currentPath) {
+            // reached root folder, throw error;
+            displayNonInitializedFolderErrorMessage();
+            throw new Error();
+        }
+
+    }
+
+    console.log(
+        clc.info("harmonic.json found at: " + currentPath)
+    );
+    return currentPath;
+}
+
 // Check if harmonic.json file exists
 function isHarmonicProject(sitePath) {
-    const clc = cliColor();
 
     try {
         getConfig(sitePath);
         return true;
     } catch (e) {
-        console.log(
-            clc.warn('It seems this is not an Harmonic project yet. \n') +
-            clc.warn('Check your directory or run ') +
-            clc.info.bgWhite.italic(' harmonic init ') +
-            clc.warn(' to start a new Harmonic project.')
-        );
+       
         return false;
     }
 }
