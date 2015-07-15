a50fd0549d2881d86515428589fa9db5d0c87572
UltCombo <ultcombo@gmail.com>
2015-07-15 00:43:19 -0300

    harmonic-client: workaround eslint bug

--- src/bin/client/harmonic-client.js
+++ src/bin/client/harmonic-client.js
@@ -7,7 +7,11 @@
 // we can just whitelist `__HARMONIC` as a global identifier
 // instead of having to whitelist every single substitution.
 
-class Harmonic {
+// TODO ESLint's `exported` directive seems to not be working correctly
+// with the current version.
+// We should probably `export` Harmonic using ES2015 module syntax and
+// trash the `exported` directive.
+class Harmonic { // eslint-disable-line no-unused-vars
 
     constructor(name) {
         this.name = name;
