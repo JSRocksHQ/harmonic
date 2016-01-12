f8fcd3665c91b64368c5b031c3f0f5071a01c04c
UltCombo <ultcombo@gmail.com>
2016-01-12 02:10:03 -0200

    test: update `should` dependency, fix tests, fix code style

--- src/test/main.js
+++ src/test/main.js
@@ -1,5 +1,4 @@
 /* eslint-env mocha */
-/* eslint-disable no-unused-expressions */ // Should.js
 
 import { spawn, exec } from 'child_process';
 import { join } from 'path';
@@ -15,37 +14,37 @@ const testDir = join(__dirname, 'site');
 const harmonicBin = join(__dirname, '../../entry_points/harmonic');
 const stdoutWrite = process.stdout.write;
 
-before(function() {
+before(() => {
     rimrafSync(testDir);
     mkdirpSync(testDir);
 });
 
-after(function() {
+after(() => {
     rimrafSync(testDir);
 });
 
 function disableStdout() {
-    process.stdout.write = function() {};
+    process.stdout.write = () => {};
 }
 function enableStdout() {
     process.stdout.write = stdoutWrite;
 }
 
-describe('CLI', function() {
-    it('should display an error for unknown commands', function(done) {
-        exec('node "' + harmonicBin + '" foobarbaz', function(error, stdout, stderr) {
+describe('CLI', () => {
+    it('should display an error for unknown commands', (done) => {
+        exec('node "' + harmonicBin + '" foobarbaz', (error, stdout, stderr) => {
             error.code.should.equal(1);
             stderr.should.containEql('foobarbaz');
             done();
         });
     });
 
-    it('should init a new Harmonic site', function(done) {
+    it('should init a new Harmonic site', (done) => {
         const harmonic = spawn('node', [harmonicBin, 'init', testDir]);
         harmonic.stdin.setEncoding('utf8');
         harmonic.stdout.setEncoding('utf8');
 
-        harmonic.stdout.on('data', function(data) {
+        harmonic.stdout.on('data', (data) => {
             if (data.indexOf('successfully created') === -1) {
                 harmonic.stdin.write('\n');
                 return;
@@ -53,117 +52,115 @@ describe('CLI', function() {
             harmonic.stdin.end();
         });
 
-        harmonic.on('close', function() {
-            isHarmonicProject(testDir).should.be.true;
+        harmonic.on('close', () => {
+            isHarmonicProject(testDir).should.be.true();
             done();
         });
     });
 
-    it('should build the Harmonic site', function(done) {
+    it('should build the Harmonic site', (done) => {
         const harmonic = spawn('node', [harmonicBin, 'build', testDir]);
         harmonic.stdin.setEncoding('utf8');
         harmonic.stdout.setEncoding('utf8');
 
-        harmonic.on('close', function() {
-            existsSync(join(testDir, 'public')).should.be.true;
+        harmonic.on('close', () => {
+            existsSync(join(testDir, 'public')).should.be.true();
             done();
         });
     });
 
-    it('should create and build a new post', function(done) {
-        const config = getConfig(testDir),
-            langs = config.i18n.languages,
-            title = 'new_post test',
-            fileName = titleToFilename(title),
-            harmonic = spawn('node', [harmonicBin, 'new_post', '--no-open', title, testDir]);
+    it('should create and build a new post', async () => {
+        const config = getConfig(testDir);
+        const langs = config.i18n.languages;
+        const title = 'new_post test';
+        const fileName = titleToFilename(title);
+        const harmonic = spawn('node', [harmonicBin, 'new_post', '--no-open', title, testDir]);
         harmonic.stdin.setEncoding('utf8');
         harmonic.stdout.setEncoding('utf8');
 
-        new Promise(function(resolve) {
-            harmonic.on('close', function() {
-                langs.forEach(function(lang) {
+        await new Promise((resolve) => {
+            harmonic.on('close', () => {
+                for (const lang of langs) {
                     readFileSync(
                         join(testDir, postspath, lang, fileName)
                     ).toString().should.containEql(title);
-                });
+                }
                 resolve();
             });
-        }).then(function() {
-            const harmonicBuild = spawn('node', [harmonicBin, 'build', testDir]);
-            harmonicBuild.stdin.setEncoding('utf8');
-            harmonicBuild.stdout.setEncoding('utf8');
-            return new Promise(function(resolve) {
-                harmonicBuild.on('close', function() {
-                    const date = new Date(),
-                        year = String(date.getFullYear()),
-                        month = ('0' + (date.getMonth() + 1)).slice(-2),
-                        slug = fileName.replace(/\.md$/, '');
-                    langs.forEach(function(lang) {
-                        const langSegment = lang === config.i18n.default ? '.' : lang;
-                        readFileSync(join(testDir, 'public', langSegment, year, month,
-                            slug, 'index.html')).toString().should.containEql(title);
-                    });
-                    resolve();
-                });
+        });
+
+        const harmonicBuild = spawn('node', [harmonicBin, 'build', testDir]);
+        harmonicBuild.stdin.setEncoding('utf8');
+        harmonicBuild.stdout.setEncoding('utf8');
+        await new Promise((resolve) => {
+            harmonicBuild.on('close', () => {
+                const date = new Date(),
+                    year = String(date.getFullYear()),
+                    month = ('0' + (date.getMonth() + 1)).slice(-2),
+                    slug = fileName.replace(/\.md$/, '');
+                for (const lang of langs) {
+                    const langSegment = lang === config.i18n.default ? '.' : lang;
+                    readFileSync(join(testDir, 'public', langSegment, year, month,
+                        slug, 'index.html')).toString().should.containEql(title);
+                }
+                resolve();
             });
-        }).then(done);
+        });
     });
 
-    it('should create and build a new page', function(done) {
-        const config = getConfig(testDir),
-            langs = config.i18n.languages,
-            title = 'new_page test',
-            fileName = titleToFilename(title),
-            harmonic = spawn('node', [harmonicBin, 'new_page', '--no-open', title, testDir]);
+    it('should create and build a new page', async () => {
+        const config = getConfig(testDir);
+        const langs = config.i18n.languages;
+        const title = 'new_page test';
+        const fileName = titleToFilename(title);
+        const harmonic = spawn('node', [harmonicBin, 'new_page', '--no-open', title, testDir]);
         harmonic.stdin.setEncoding('utf8');
         harmonic.stdout.setEncoding('utf8');
 
-        new Promise(function(resolve) {
-            harmonic.on('close', function() {
-                langs.forEach(function(lang) {
+        await new Promise((resolve) => {
+            harmonic.on('close', () => {
+                for (const lang of langs) {
                     readFileSync(
                         join(testDir, pagespath, lang, fileName)
                     ).toString().should.containEql(title);
-                });
+                }
                 resolve();
             });
-        }).then(function() {
-            const harmonicBuild = spawn('node', [harmonicBin, 'build', testDir]);
-            harmonicBuild.stdin.setEncoding('utf8');
-            harmonicBuild.stdout.setEncoding('utf8');
-            return new Promise(function(resolve) {
-                harmonicBuild.on('close', function() {
-                    const slug = fileName.replace(/\.md$/, '');
-                    langs.forEach(function(lang) {
-                        const langSegment = lang === config.i18n.default ? '.' : lang;
-                        readFileSync(join(testDir, 'public', langSegment, 'pages',
-                            slug, 'index.html')).toString().should.containEql(title);
-                    });
-                    resolve();
-                });
+        });
+
+        const harmonicBuild = spawn('node', [harmonicBin, 'build', testDir]);
+        harmonicBuild.stdin.setEncoding('utf8');
+        harmonicBuild.stdout.setEncoding('utf8');
+        await new Promise((resolve) => {
+            harmonicBuild.on('close', () => {
+                const slug = fileName.replace(/\.md$/, '');
+                for (const lang of langs) {
+                    const langSegment = lang === config.i18n.default ? '.' : lang;
+                    readFileSync(join(testDir, 'public', langSegment, 'pages',
+                        slug, 'index.html')).toString().should.containEql(title);
+                }
+                resolve();
             });
-        }).then(done);
+        });
     });
 });
 
-describe('helpers', function() {
-
-    it('.isHarmonicProject() should return whether the CWD is a Harmonic site', function() {
+describe('helpers', () => {
+    it('.isHarmonicProject() should return whether the CWD is a Harmonic site', () => {
         disableStdout();
         const result = isHarmonicProject(__dirname);
         enableStdout();
-        result.should.be.false;
-        isHarmonicProject(testDir).should.be.true;
+        result.should.be.false();
+        isHarmonicProject(testDir).should.be.true();
     });
 
-    it('.titleToFilename() should transform a post/page title into a filename', function() {
+    it('.titleToFilename() should transform a post/page title into a filename', () => {
         titleToFilename('Hello World!').should.equal('hello-world.md');
     });
 });
 
-describe('API', function() {
-
-    it('should merge the theme\'s config into the main config', function() {
+describe('API', () => {
+    it('should merge the theme\'s config into the main config', () => {
         const config = getConfig(testDir);
         const themeConfigPath = join(testDir, 'node_modules', config.theme, 'config.json');
         const templateConfig = { customData: 'test' };
